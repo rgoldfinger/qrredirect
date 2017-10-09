dbabebec3997d12463ac8ae11dc709af845d4687
Roger Goldfinger <rgoldfinger@gmail.com>
2017-10-08 23:07:38 -0700

    setup heroku

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get('/', function (req, res) {
   res.redirect(301, url);
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
